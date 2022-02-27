a712894ac1480fca078ed90c7809383a61a7385d
AbrarovaA <abrarova@hotmail.com>
2022-02-27 21:10:20 +0300

    Добавлены LoginTests

--- addressbook-web-tests/addressbook-web-tests/appmanager/LoginHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/LoginHelper.cs
@@ -32,7 +32,11 @@ namespace WebAddressbookTests
         }
         public void Logout()
         {
-            driver.FindElement(By.LinkText("Logout")).Click();
+            if (IsLoggedIn())
+            {
+                driver.FindElement(By.LinkText("Logout")).Click();
+            }
+            
         }       
         public bool IsLoggedIn()
         {
