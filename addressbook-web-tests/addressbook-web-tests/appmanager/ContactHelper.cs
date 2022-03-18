30b6135020707e422dc7874b802f91c75811e931
AbrarovaA <abrarova@hotmail.com>
2022-03-18 19:02:54 +0300

    Создан ContactInformationTest

--- addressbook-web-tests/addressbook-web-tests/appmanager/ContactHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/ContactHelper.cs
@@ -15,7 +15,7 @@ namespace WebAddressbookTests
             : base(manager)
         {           
         }
-
+       
         public ContactHelper CreateContact(ContactData contact)
         {
             InitContactCreation();
@@ -24,12 +24,11 @@ namespace WebAddressbookTests
             ReturnToHomePage();
             return this;
         }
-
        
         public ContactHelper Modify(int p, ContactData newData)
         {            
             SelectContact(p);
-            InitContactModification();
+            InitContactModification(0);
             FillContactForm(newData);
             SubmitContactModification();
             ReturnToHomePage();
@@ -120,9 +119,11 @@ namespace WebAddressbookTests
             return this;
         }
 
-        public ContactHelper InitContactModification()
+        public ContactHelper InitContactModification(int index)
         {
-            driver.FindElement(By.XPath("//img[@alt='Edit']")).Click();
+            driver.FindElements(By.Name("entry"))[index]
+                .FindElements(By.TagName("td"))[7]
+                .FindElement(By.TagName("a")).Click();
             return this;
         }
 
@@ -144,6 +145,44 @@ namespace WebAddressbookTests
             }
             return new List<ContactData>(contactCash);
         }
-       
+        public ContactData GetContactInformationFromTable(int index)
+        {
+            manager.Navigator.GoToHomePage();
+
+            IList<IWebElement> cells = driver.FindElements(By.Name("entry"))[index]
+                .FindElements(By.TagName("td"));
+            string lastName = cells[1].Text;
+            string firstName = cells[2].Text;
+            string address = cells[3].Text;
+            string allPhones = cells[5].Text;
+
+            return new ContactData(firstName, lastName)
+            {
+                Address = address,
+                AllPhones = allPhones,
+            };
+        }
+
+        public ContactData GetContactInformationFromEditForm(int index)
+        {
+            manager.Navigator.GoToHomePage();
+            InitContactModification(0);
+            string firstName = driver.FindElement(By.Name("firstname")).GetAttribute("value");
+            string lastName = driver.FindElement(By.Name("lastname")).GetAttribute("value");
+            string address = driver.FindElement(By.Name("address")).GetAttribute("value");
+
+            string homePhone = driver.FindElement(By.Name("home")).GetAttribute("value");
+            string mobilePhone = driver.FindElement(By.Name("mobile")).GetAttribute("value");
+            string workPhone = driver.FindElement(By.Name("work")).GetAttribute("value");
+
+            return new ContactData(firstName, lastName)
+            {
+                Address = address,
+                Home = homePhone,
+                Mobile = mobilePhone,
+                Work = workPhone
+            };
+        }
+
     }
 }
